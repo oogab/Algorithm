2b37d78f83bae05a70514ca1e5f98ed4a94903bb
백상욱 <oogab@naver.com>
2021-10-19 15:25:54 +0900

    초기 커밋.

---
+++ Algorithm/jo_505.cpp
@@ -0,0 +1,11 @@
+//// #include <iostream>
+//#include <cstdio>
+//
+//// using namespace std;
+//
+//int main(void) {
+//	printf("%s", "I can program well.\n");
+//	printf("%s", "Dreams come true.\n");
+//	
+//	return 0;
+////
